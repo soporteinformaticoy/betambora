a1194fb3bb60f8611736f01085ca9f10be7e0a7d
soporteinformaticoy <media.soporte.informatico@gmail.com>
2025-11-30 19:56:08 -0300

    fix(google-oauth): callbackURL debe ser URL completa para evitar redirect_uri_mismatch

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
